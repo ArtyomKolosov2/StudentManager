363bd38c1e4ab64e2a5f6fe1c04447a9eb8f8d61
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-01-28 15:12:39 +0300

    Test commit

--- StudentManager_API/student-client/src/App.js
+++ StudentManager_API/student-client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import { Route } from 'react-router';
+import { About } from './components/about';
 
-function App() {
-  return (
+export default class App extends Component {
+  render (){ return (
     <div className="App">
+      <Route path='/about' component={About} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+  }
 }
-
-export default App;
